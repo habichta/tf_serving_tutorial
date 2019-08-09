0f0fae061eab74dbbafa9be3dcffbaa443b2945d
Arthur <arthurhabicht88@gmail.com>
2019-08-09 21:30:55 +0200

    [added] version labels

--- serve.sh
+++ serve.sh
@@ -1,8 +1,7 @@
 docker run -t --rm -p 8501:8501 \
     -v "$(pwd)/models/":"/models/" \
-    tensorflow/serving \
+    tensorflow/serving:nightly \
     --model_config_file=/models/models.config \
-    --monitoring_config_file=/models/monitoring.config \
-    --model_config_file_poll_wait_seconds=60 \
-    --allow_version_labels_for_unavailable_model=1 \
+    --model_config_file_poll_wait_seconds=5 \
+    --allow_version_labels_for_unavailable_models
 
