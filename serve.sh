ca4e7343f453f915ba6cdfa4f1893c33d9e633fa
Arthur <arthurhabicht88@gmail.com>
2019-08-11 18:27:36 +0200

    [added] prometheus config

--- serve.sh
+++ serve.sh
@@ -2,6 +2,7 @@ docker run -t --rm -p 8501:8501 \
     -v "$(pwd)/models/":"/models/" \
     tensorflow/serving:nightly \
     --model_config_file=/models/models.config \
+    --monitoring_config_file=/models/monitoring.config \
     --model_config_file_poll_wait_seconds=5 \
     --allow_version_labels_for_unavailable_models
 
